d6bd57b0f220a1dc60ef06010e14c0b07f8a6e2f
Lucy Toman <lucy.toman@thinslices.com>
2025-01-15 19:29:55 +0100

    updates packages and minor style tweaks

--- src/count.js
+++ src/count.js
@@ -1,85 +1,85 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 export default function Count(props) {
-    const title = props.title;
+  const title = props.title;
 
-    const [number, setNumber] = useState(0);
-    const [inc, setInc] = useState(1);
+  const [number, setNumber] = useState(0);
+  const [inc, setInc] = useState(1);
 
-    let i = number;
-    let x;
+  let i = number;
+  let x;
 
-    function add() {
-        i = number + inc;
+  function add() {
+    i = number + inc;
 
-        setNumber(i);
+    setNumber(i);
+  }
+
+  function minus() {
+    if (i > 0) {
+      i--;
+
+      setNumber(i);
+    } else {
+      return;
     }
+  }
 
-    function minus() {
-        if (i > 0) {
-            i--;
+  function clear() {
+    i = 0;
+    setNumber(i);
+    x = 1;
+    setInc(x);
+    document.getElementById('increment').value = '';
+  }
 
-            setNumber(i);
-        } else {
-            return;
-        }
-    }
+  function setIncrement() {
+    x = document.getElementById('increment').value;
+    x = parseInt(x);
 
-    function clear() {
-        i = 0;
-        setNumber(i);
-        x = 1;
-        setInc(x);
-        document.getElementById("increment").value = "";
-    }
+    setInc(x);
+  }
 
-    function setIncrement() {
-        x = document.getElementById("increment").value;
-        x = parseInt(x);
-
-        setInc(x);
-    }
-
-    return (
-        <div className="click-container">
-            <div className="title">{title} </div>
-            {title === "Stitches" && (
-                <div className="settings">
-                    <div className="st-text no-box">
-                        <p>count in</p>
-                        <p>multiples of:</p>
-                    </div>
-                    <input
-                        id="increment"
-                        className="box"
-                        type="number"
-                        name="increment"
-                        min="0"
-                        placeholder="1"
-                    />
-                    <button className="box submit" onClick={setIncrement}>
-                        confirm
-                    </button>
-                </div>
-            )}
-            <div className="counter">
-                <div className="box">
-                    <button className="minus" onClick={minus}>
-                        -
-                    </button>
-                </div>
-                <div className="number box">{number}</div>
-                <div className="box">
-                    <button className="plus" onClick={add}>
-                        +
-                    </button>
-                </div>
-            </div>
-            <div className="box reset">
-                <button className="reset" onClick={clear}>
-                    reset
-                </button>
-            </div>
+  return (
+    <div className='click-container'>
+      <div className='title'>{title} </div>
+      {title === 'Stitches' && (
+        <div className='settings'>
+          <div className='st-text no-box'>
+            <p>count in</p>
+            <p>multiples of:</p>
+          </div>
+          <input
+            id='increment'
+            className='box'
+            type='number'
+            name='increment'
+            min='0'
+            placeholder='1'
+          />
+          <button className='box submit' onClick={setIncrement}>
+            set
+          </button>
         </div>
-    );
+      )}
+      <div className='counter'>
+        <div className='box'>
+          <button className='minus' onClick={minus}>
+            -
+          </button>
+        </div>
+        <div className='number box'>{number}</div>
+        <div className='box'>
+          <button className='plus' onClick={add}>
+            +
+          </button>
+        </div>
+      </div>
+      <div className='box reset'>
+        <button className='reset' onClick={clear}>
+          reset
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/counter.js
+++ src/counter.js
@@ -1,12 +1,12 @@
-import React from "react";
-import Count from "./count";
+import React from 'react';
+import Count from './count';
 
 export default function Counter() {
-    return (
-        <>
-            <Count title={"Rows"} />
-            <Count title={"Stitches"} />
-            <Count title={"Pattern Rpt"} />
-        </>
-    );
+  return (
+    <>
+      <Count title={'Rows'} />
+      <Count title={'Stitches'} />
+      <Count title={'Pattern Repeat'} />
+    </>
+  );
 }
