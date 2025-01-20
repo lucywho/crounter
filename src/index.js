58fb159d60f5e37cbfc98f8bd150af2ab59a89c3
Lucy Toman <lucy.toman@thinslices.com>
2025-01-20 09:54:55 +0100

    updates index.js and index.html

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import App from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.querySelector("main")
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
